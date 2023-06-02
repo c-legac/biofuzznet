a904aaea9813005732877871bc53c952f976d653
Alice Driessen <adr@zurich.ibm.com>
2023-06-02 03:10:25 -0400

    Running subnetwork on allmost all cell lines
    Signed-off-by: Alice Driessen <adr@zurich.ibm.com>

--- biological_fuzzy_logic_networks/DREAM_analysis/submit_configs.sh
+++ biological_fuzzy_logic_networks/DREAM_analysis/submit_configs.sh
@@ -3,7 +3,7 @@ source /opt/share/anaconda3-2019.03/x86_64/etc/profile.d/conda.sh
 conda activate biofuzznet2
 echo $CONDA_DEFAULT_ENV
 
-config_folder=/dccstor/ipc1/CAR/DREAM/Model/Test/MEK_FAK_ERK/Configs/
+config_folder=/dccstor/ipc1/CAR/DREAM/Model/Test/Subnetwork/Configs/
 
 for file in $config_folder*.json
     do 
