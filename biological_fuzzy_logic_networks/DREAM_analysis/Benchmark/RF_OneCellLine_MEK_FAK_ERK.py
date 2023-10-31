ea0206f2cb7efce3d7329f4fe95aa6680b1cf2e2
Alice Driessen <adr@zurich.ibm.com>
2023-10-31 06:50:00 -0400

    CCC update
    Signed-off-by: Alice Driessen <adr@zurich.ibm.com>

--- biological_fuzzy_logic_networks/DREAM_analysis/Benchmark/RF_OneCellLine_MEK_FAK_ERK.py
+++ biological_fuzzy_logic_networks/DREAM_analysis/Benchmark/RF_OneCellLine_MEK_FAK_ERK.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import OneHotEncoder
+
+# from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
@@ -78,28 +79,27 @@ data.loc[data["treatment"] == "full", "stimulation"] = 0
 data["starvation"] = 1
 data.loc[data["treatment"] == "full", "starvation"] = 0
 
-cat_data = pd.DataFrame(data["treatment"])
-encoder = OneHotEncoder()
-cat_data_encoded = encoder.fit_transform(cat_data)
-data[np.concatenate(encoder.categories_, axis=0)] = cat_data_encoded.todense()
+# cat_data = pd.DataFrame(data["treatment"])
+# encoder = OneHotEncoder()
+# cat_data_encoded = encoder.fit_transform(cat_data)
+# data[np.concatenate(encoder.categories_, axis=0)] = cat_data_encoded.todense()
 # data = data.drop("treatment", axis=1)
 
-cat_features = list(np.concatenate(encoder.categories_, axis=0)) + [
+# cat_features = list(np.concatenate(encoder.categories_, axis=0)) + [
+#     "starvation",
+#     "stimulation",
+# ]
+
+cat_features = [
     "starvation",
     "stimulation",
 ]
 
+
 train, test = train_test_split(data)
 
-np.save(
-    "/dccstor/ipc1/CAR/DREAM/Model/Baseline/RF_OneCellLine_MEK_FAK_ERK_MF_inputs/RF_OneCellLine_train.npy",
-    train,
-)
-
-np.save(
-    "/dccstor/ipc1/CAR/DREAM/Model/Baseline/RF_OneCellLine_MEK_FAK_ERK_MF_inputs/RF_OneCellLine_test.npy",
-    test,
-)
+train.to_csv("/dccstor/ipc1/CAR/DREAM/Model/Baseline/RF_OneCellLine_MEK_FAK_ERK_MF_inputs_without_tr/RF_OneCellLine_train.csv")
+test.to_csv("/dccstor/ipc1/CAR/DREAM/Model/Baseline/RF_OneCellLine_MEK_FAK_ERK_MF_inputs_without_tr/RF_OneCellLine_test.csv")
 
 for marker_to_predict in markers_to_predict:
     print(marker_to_predict)
@@ -110,7 +110,7 @@ for marker_to_predict in markers_to_predict:
     pred = rf.predict(test[features])
 
     np.save(
-        f"/dccstor/ipc1/CAR/DREAM/Model/Baseline/RF_OneCellLine_MEK_FAK_ERK_MF_inputs/{marker_to_predict}.npy",
+        f"/dccstor/ipc1/CAR/DREAM/Model/Baseline/RF_OneCellLine_MEK_FAK_ERK_MF_inputs_without_tr/{marker_to_predict}.npy",
         pred,
     )
     print(rf.score(test[features], test[marker_to_predict]))
