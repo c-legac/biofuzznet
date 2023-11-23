3b29d046c3e28923dc70671b0ac4039fea46f914
Nicolas Deutschmann <deu@zurich.ibm.com>
2023-11-23 13:08:20 +0100

    class mixin for label shuffle
    Signed-off-by: Nicolas Deutschmann <deu@zurich.ibm.com>

---
+++ biological_fuzzy_logic_networks/label_shuffle.py
@@ -0,0 +1,44 @@
+import random
+
+import torch
+
+
+class LabelShuffleMixin:
+    """A BFN mixin that remaps all biological nodes in the network."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        names = list(self.biological_nodes)
+        # random.sample is without replacement
+        shuffled_names = random.sample(names, k=len(names))
+        self.node_mapping = dict(zip(names, shuffled_names))
+
+    def conduct_optimisation(
+            self,
+            input: dict,
+            ground_truth: dict,
+            test_input: dict,
+            test_ground_truth: dict,
+            epochs: int,
+            batch_size: int,
+            learning_rate: float,
+            optim_wrapper=torch.optim.Adam,
+    ):
+        all_data = dict(**input, **ground_truth)
+        all_test_data = dict(**test_input, **test_ground_truth)
+
+        new_input = {k: all_data[self.node_mapping[k]] for k in input}
+        new_ground_truth = {k: all_data[self.node_mapping[k]] for k in ground_truth}
+        new_test_input = {k: all_test_data[self.node_mapping[k]] for k in test_input}
+        new_test_ground_truth = {k: all_data[self.node_mapping[k]] for k in test_ground_truth}
+
+        return super(LabelShuffleMixin, self).conduct_optimisation(
+            input=new_input,
+            ground_truth=new_ground_truth,
+            test_input=new_test_input,
+            test_ground_truth=new_test_ground_truth,
+            epochs=epochs,
+            batch_size=batch_size,
+            learning_rate=learning_rate,
+            optim_wrapper=optim_wrapper
+        )
