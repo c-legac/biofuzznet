0cfb89e3ffc79ed25aee1b00b383a89ebeaadcbd
Nicolas Deutschmann <deu@zurich.ibm.com>
2023-11-23 14:21:25 +0100

    change level of action for node shuffling
    Signed-off-by: Nicolas Deutschmann <deu@zurich.ibm.com>

--- biological_fuzzy_logic_networks/label_shuffle.py
+++ biological_fuzzy_logic_networks/label_shuffle.py
@@ -1,50 +1,29 @@
 import random
+from collections.abc import Sequence
 from typing import Type
-
-import torch
 
 from biological_fuzzy_logic_networks.biofuzznet import BioFuzzNet
 
 
-class LabelShuffleMixin:
-    """A BFN mixin that remaps all biological nodes in the network."""
+class LabelShuffleMixin(BioFuzzNet):
+    """A BFN mixin that remaps biological nodes in the network (only those with a ground truth)."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        names = list(self.biological_nodes)
-        # random.sample is without replacement
-        shuffled_names = random.sample(names, k=len(names))
-        self.node_mapping = dict(zip(names, shuffled_names))
+        self.node_mapping = None
 
-    def conduct_optimisation(
-            self,
-            input: dict,
-            ground_truth: dict,
-            test_input: dict,
-            test_ground_truth: dict,
-            epochs: int,
-            batch_size: int,
-            learning_rate: float,
-            optim_wrapper=torch.optim.Adam,
-    ):
-        all_data = dict(**input, **ground_truth)
-        all_test_data = dict(**test_input, **test_ground_truth)
+    def setup_node_mapping(self, node_names: Sequence[str]):
+        shuffled_names = random.sample(node_names, k=len(node_names))
+        self.node_mapping = dict(zip(node_names, shuffled_names))
 
-        new_input = {k: all_data[self.node_mapping[k]] for k in input}
-        new_ground_truth = {k: all_data[self.node_mapping[k]] for k in ground_truth}
-        new_test_input = {k: all_test_data[self.node_mapping[k]] for k in test_input}
-        new_test_ground_truth = {k: all_data[self.node_mapping[k]] for k in test_ground_truth}
+    def set_network_ground_truth(self, ground_truth):
+        if self.node_mapping is None:
+            self.setup_node_mapping(tuple(ground_truth.keys()))
 
-        return super(LabelShuffleMixin, self).conduct_optimisation(
-            input=new_input,
-            ground_truth=new_ground_truth,
-            test_input=new_test_input,
-            test_ground_truth=new_test_ground_truth,
-            epochs=epochs,
-            batch_size=batch_size,
-            learning_rate=learning_rate,
-            optim_wrapper=optim_wrapper
-        )
+        assert self.node_mapping.keys() == ground_truth.keys(), \
+            "The mapping for node shuffling should match the ground truth"
+
+        return super().set_network_ground_truth({k: ground_truth[self.node_mapping[k]] for k in ground_truth})
 
 
 def create_shuffled_subclass(base_cls: Type[BioFuzzNet]):
@@ -53,4 +32,3 @@ def create_shuffled_subclass(base_cls: Type[BioFuzzNet]):
     """
     shuffled_subclass = type(f"Shuffled{base_cls.__name__}", (LabelShuffleMixin, base_cls), {})
     return shuffled_subclass
-
